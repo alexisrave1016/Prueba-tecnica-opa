df9cb936fa382fb97aaf92e3aa44c6aad6eae1e3
alexisRave <alexis9876@hotmail.com>
2022-07-27 02:17:24 -0500

    actualizacion

--- main.js
+++ main.js
@@ -1,18 +1,17 @@
-// const prueba=parseInt(document.getElementsByClassName('input').value);
-// console.log('malparido hp'+ prueba)
 const miClick=()=>{
     const numeroImportado= document.querySelector(".input").value;
             document.querySelector(".resultado").innerHTML=numeroImportado; 
             const numero=parseInt(numeroImportado)
+            
            
-            if(numeroImportado==''){
-                document.querySelector('.resultado').innerHTML=('Ingrese número');
-            }
-            if(numero <= 0){
-                document.querySelector('.resultado').innerHTML=('El número ingresado debe de ser positivo');
-            }
-            if(typeof numero=='string'){document.querySelector(".resultado").innerHTML=numero;
-            }
+            // if(numero==''){
+            //     document.querySelector('.prueba').innerHTML=('Ingrese número');
+            // }
+            // if(numero <= 0){
+            //     document.querySelector('.prueba').innerHTML=('El número ingresado debe de ser positivo');
+            // }
+            // if(typeof numero=='number'){document.querySelector(".prueba").innerHTML=numero;
+            // }
 
             let suma= 0;
             for (let i = 1; i <=numero/2; ++i) {
@@ -29,52 +28,10 @@ const miClick=()=>{
 
     }
     
-    
-
-
-
-
-// aca estoy medio bien
-
-
-
-
-
-
-
-// const numeroPerfecto=(numero)=>{
-
-//     if(!Number.isInteger(numero)){
-//         throw TypeError('El número ingresado debe de ser un entero');
-//     }
-//     if(numero <= 0){
-//         console.log('El Numero ingresaso debe de ser un numero positivo');
-//     }
-
-//     let suma= 0;
-//     for (let i = 1; i <= numero/2; ++i) {
-//         if(numero % i == 0){
-//             suma += i;
-//         }
-//     }
-
-//     return suma != 0 && suma== numero;
-
-// }
-
-
-
-// try{console.log(numeroPerfecto(6))
-// }catch(e){
-//     console.log(`Error:`)
-// }
-
-// try{console.log(numeroPerfecto(28))
-// }catch(e){
-//     console.log(`Error:`)
-// } 
-
-
+const borrar=()=>{
+    const borrarInput= document.querySelector('.input').value='';
+        document.querySelector('.resultado').innerHTML=''
+}    
 
 
 
